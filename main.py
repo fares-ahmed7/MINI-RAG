ec3c5fd92b118e40cff4a733b6275acfec07797f
Fares Ahmed <faresdahawy7@gmail.com>
2026-03-05 21:02:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
+
 app = FastAPI()
 
-@app.get("/welcome")
-
-def welcome():
-    return {
-        "message": "Welcome to the mini-RAG API!"
-    }
+app.include_router(base.base_router)
