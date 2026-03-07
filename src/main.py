2b85504ae5f48f5822384d6c1355a943958ca95a
Fares Ahmed <faresdahawy7@gmail.com>
2026-03-07 02:37:26 +0200

    setup database schemes

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
-from fastapi.concurrency import asynccontextmanager
+from contextlib import asynccontextmanager
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
-app = FastAPI()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    
     settings = get_settings()
-    app.mongodb_client = AsyncIOMotorClient(settings.MONGODB_URI)
-    app.db_client = app.mongodb_client[settings.MONGODB_DATABASE]
+   
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URI) 
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
     
-    yield  
+    yield
     
-    app.mongodb_client.close()    
+    app.mongo_conn.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
